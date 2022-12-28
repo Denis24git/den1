79d090585303a44271fd8d72ab73a284c7f19d4e
unknown <rokkerden@gmail.com>
2022-12-28 02:36:28 +0200

    first commit

---
+++ new_prjs/main.js
@@ -0,0 +1,60 @@
+"use strict";
+// console.log(1);
+// let freu = "fre";
+// console.log(freu);
+// // alert("пошёл нахуй" );
+// const gopert = 10;
+// freu = "234";
+// console.log(freu);
+// const tyi = {
+//     a: 50
+// };
+// tyi.a = "ret";
+// console.log(tyi);
+// // объект
+// let sdf = {
+//     name: "botis",
+//     age: 23,
+//     jir: "68kg"
+// }
+
+
+
+// sdf ["1234"] = "potr";
+// console.log(sdf ["1234"])
+// console.log(sdf.age);
+// const b = "www";
+// sdf["b"] = "frooooo";
+
+// console.log(sdf["b"])
+// // массив, он является разновидностью объекта
+// let rtoa = ["der", "ver", "her",6,7 ];
+// console.log(rtoa[3]);
+// const result = confirm("вы готовы?");
+// console.log(result);
+// const answer = +prompt ("вам есть 18", "18");
+// console.log(answer + 5);
+const answers = [];
+// answers[0] = prompt(" Your name","");
+// answers[1] = prompt(" Your family","");
+// answers[2] = prompt(" Your age","");
+// document.write(answers);
+console.log(typeof(answers));
+const category = ("toys");
+console.log (`https://someurl.com/${category}/5`); 
+const user = 'ivan';
+alert(`привет ${user}`);
+let incr = 10,
+    decr = 10;
+    // incr++;
+    // decr--;
+    // console.log(incr++);
+    // console.log(decr--); 
+    // console.log(5%2);
+    
+    // console.log( 2*4 === '8');
+    
+    const isChecked = true,
+    isClouse = false;
+    console.log(isChecked && isClouse )
+    console.log(2 + 2 * 2 === '6');
